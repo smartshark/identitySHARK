20bbdf462cf352e3f5cdd31090fabf363485c426
sherbold <herbold@cs.uni-goettingen.de>
2018-03-14 08:42:47 +0100

    Made post-merge faster at the cost of memory

--- post_merge.py
+++ post_merge.py
@@ -1,27 +1,55 @@
+import sys
+import timeit
+
 from mongoengine import connect
 from pycoshark.mongomodels import Identity
 from pycoshark.utils import create_mongodb_uri_string
+
+start_time = timeit.default_timer()
 
 user = ''
 password = ''
 host = ''
 port = ''
 authentication_db = ''
-database = "smartshark_test"
+database = ""
 ssl_enabled = None
 
 uri = create_mongodb_uri_string(user, password, host, port, authentication_db, ssl_enabled)
 connect(database, host=uri)
 
-# Get all identities
+print("fetching identities...")
+# identities = list(Identity.objects[:200000])
 identities = list(Identity.objects.all())
+print("%s identities found" % len(identities))
+peopleset = set()
 for identity in identities:
-    # Check if another identity with the same people exist already
-    exist_identity = Identity.objects(people__size=len(identity.people), people__all=identity.people).all()
-    if len(exist_identity) > 1:
-        for found_identity in exist_identity:
-            if found_identity.id != identity.id:
-                print("Identity %s equals Identity %s" % (identity.id, found_identity.id))
-                print("Identity people: %s" % identity.people)
-                print("Deleting identity with people: %s" % found_identity.people)
-                Identity.objects(id=found_identity.id).delete()
+    people = tuple(sorted(list(identity.people)))
+    if len(people) > 2:
+        print("found identity with more than two people: ", people)
+        print("aborting")
+        sys.exit()
+    peopleset.add(people)
+
+print("%i identities in peopleset" % len(peopleset))
+
+print("deleting current identites")
+Identity.objects().delete()
+
+print("creating new identities")
+identities_to_store = []
+for people in peopleset:
+    identity = Identity()
+    identity.people = list(people)
+    identities_to_store.append(identity)
+    if len(identities_to_store) >= 100000:
+        # store in between to keep size of inserts under control
+        Identity.objects.insert(identities_to_store)
+        identities_to_store = []
+
+# store remaining identities
+Identity.objects.insert(identities_to_store)
+
+print("done")
+elapsed = timeit.default_timer() - start_time
+print("Execution time: %0.5f s" % elapsed)
